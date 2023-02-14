49580e7602fae6daa35509a733b2b0976f741f16
Maria Suarez <msuarezh@gmail.com>
2023-02-14 21:50:44 +0100

    iteration 2 incomplete

--- src/index.js
+++ src/index.js
@@ -23,13 +23,20 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  const products = document.querySelectorAll('.product')
+  let priceTotal = 0;
+  for(const productElem of products) {
+    updateSubtotal(productElem);
+    console.log(updateSubtotal(productElem));
+    priceTotal += updateSubtotal(productElem);
+  }
+  return priceTotal;
   // ITERATION 3
   //... your code goes here
 }
