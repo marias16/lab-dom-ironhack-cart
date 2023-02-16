43e9e62652f28795cdc28742235516d5915252a8
Maria Suarez <msuarezh@gmail.com>
2023-02-16 18:43:15 +0100

    iteration 5 done

--- src/index.js
+++ src/index.js
@@ -53,8 +53,27 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  //we get the value of the two inputs
+  let productName = document.querySelector("#product-name").value;
+  let productPrice = document.querySelector("#product-price").value;
+  
+  //we clone the node tr class product and set the characteristics
+  let rowProduct = document.querySelector(".product").cloneNode(true);
+  rowProduct.querySelector(".name span").innerHTML = productName;
+  rowProduct.querySelector(".price span").innerHTML = productPrice;
+  rowProduct.querySelector(".quantity input").value = 0;
+
+  //we append the node to the table
+  document.querySelector("tbody").appendChild(rowProduct);
+
+  //we reset the input at create product after appending
+  document.querySelector("#product-price").value = 0;
+  document.querySelector("#product-name").value = "";
 }
+
+let buttonCreate = document.querySelector("#create");
+
+buttonCreate.addEventListener('click', createProduct)
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
